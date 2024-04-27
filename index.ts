e75d754495ef5b24d784b70a538c3f71ba26a80a
FrankieMarie <frankiemarie83@gmail.com>
2024-04-26 20:02:26 -0500

    get tasks route

--- index.ts
+++ index.ts
@@ -1,5 +1,18 @@
 import * as schema from "./schema.js";
 import { db } from "./db.js";
 
-const result = await db.select().from(schema.tasks);
-console.log(result);
+const server = Bun.serve({
+  async fetch(req) {
+    const path = new URL(req.url).pathname;
+
+    // GET all tasks
+    if (path === "/tasks") {
+      const result = await db.select().from(schema.tasks);
+      return Response.json({ success: true, data: result });
+    }
+
+    return new Response("Not found.", { status: 404 });
+  },
+});
+
+console.log(`Listening on ${server.url}`);
